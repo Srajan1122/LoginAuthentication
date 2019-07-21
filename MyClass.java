79f366f289668dad0e4770947b0c46ecdd18ded3
Srajan1122 <53133493+Srajan1122@users.noreply.github.com>
2019-07-21 09:31:22 +0530

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,18 @@
+public class MyClass {
+	int speed;
+	public MyClass(){
+		speed = 25;
+	}
+	public void car(){
+		System.out.println("this is inside the function");
+	}
+	
+	public static void main(String[] args){
+		MyClass mycar1 = new MyClass();
+		MyClass mycar2 = new MyClass();
+		mycar1.speed =20;
+		System.out.println(mycar1.speed);
+		System.out.println(mycar2.speed);
+		mycar1.car();
+	} 
+}
