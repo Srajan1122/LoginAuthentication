57d6218cca064725b5707d3221d9da6f64fb6a68
AkashMaurya09 <52026057+AkashMaurya09@users.noreply.github.com>
2019-07-31 19:17:03 +0530

    Profile Pic Update

--- app/src/main/java/com/example/myapplication/Signup_form.java
+++ app/src/main/java/com/example/myapplication/Signup_form.java
@@ -1,10 +1,14 @@
 package com.example.myapplication;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -14,6 +18,8 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
 import com.google.firebase.auth.AuthResult;
@@ -21,6 +27,11 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
 
 public class Signup_form extends AppCompatActivity {
 
@@ -28,15 +39,33 @@ public class Signup_form extends AppCompatActivity {
     private EditText username,email_id,password,confirm_password ;
     private Button register;
     private FirebaseAuth firebaseAuth;
+    private FirebaseStorage firebaseStorage;
     private ImageView userProfilePic;
+    private static int PICK_IMAGE = 123;
     String uname, email, pass, conf_pass;
+    Uri imagePath;
+    private StorageReference storageReference;
 
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == PICK_IMAGE && requestCode == RESULT_OK && data.getData() != null){
+            imagePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imagePath);
+                userProfilePic.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_form);
+        setupUIViews();
 
         username =  findViewById(R.id.user_name);
         email_id =   findViewById(R.id.email_id);
@@ -46,6 +75,21 @@ public class Signup_form extends AppCompatActivity {
         userProfilePic = findViewById(R.id.ivProfilePic);
 
         firebaseAuth = FirebaseAuth.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
+
+        storageReference = firebaseStorage.getReference();
+
+        userProfilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Image"), PICK_IMAGE );
+            }
+        });
+
+
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -102,6 +146,10 @@ public class Signup_form extends AppCompatActivity {
         });
 
     }
+
+    private void setupUIViews() {
+    }
+
     public void sendEmailVerification() {
         // [START send_email_verification]
         FirebaseAuth auth = FirebaseAuth.getInstance();
@@ -126,6 +174,19 @@ public class Signup_form extends AppCompatActivity {
     private void sendUserData() {
         FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
         DatabaseReference myRef = firebaseDatabase.getReference(firebaseAuth.getUid());
+        StorageReference imageReference = storageReference.child(firebaseAuth.getUid()).child("Images").child("Profile Pic");
+        UploadTask uploadTask = imageReference.putFile(imagePath);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(Signup_form.this, "Upload Failed", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(Signup_form.this, "Upload successful", Toast.LENGTH_SHORT).show();
+            }
+        });
         UserProfile userProfile = new UserProfile(uname, email);
         myRef.setValue(userProfile);
     }
